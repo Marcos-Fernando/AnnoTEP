f5e46f79b718a6fc2d6d77526bfdc68fa283ad94
Marcos Fernando <marcosinfcont@gmail.com>
2024-01-26 22:12:57 -0300

    update option download

--- www/static/scripts/main/dark-theme.js
+++ www/static/scripts/main/dark-theme.js
@@ -1,12 +1,13 @@
 //------------------  Script Dark Theme ---------------//
 var replaceConst = document.querySelector('.replace');
-var elementsWithDarkTheme = document.querySelectorAll('.main, .container-main, .box-checkboxs, .box-explicative, .title-input, .session-title, .header, .footer, .rectangle-green, .uploaddata, .img-dna, .img-phylogeny');
+var elementsWithDarkTheme = document.querySelectorAll('.main, .Download, .container-main, .box-checkboxs, .box-explicative, .title-input, .session-title, .header, .footer, .rectangle-green, .uploaddata, .img-dna, .img-phylogeny, .item-download, .icon-download, .description-download');
 var iconHelps = document.querySelectorAll('.icon-help');
 var cloudImg = document.getElementById('cloudImage');
-var logoImg = document.getElementById('logoImage');
+var logoImgs = document.querySelectorAll('.logo');
 var dnaImg = document.querySelector('.img-dna');
 var phylogenyImg = document.querySelector('.img-phylogeny');
 var emailImg = document.querySelector('.img-email');
+var shdownloads = document.querySelectorAll('.sh-download');
 
 var isCloudSun = false;
 
@@ -19,7 +20,6 @@ replaceConst.addEventListener('click', function(){
   //Se isCloudSun for true, ele usará os valores no lado esquerdo do :; caso contrário, usará os valores no lado direito do :
   isCloudSun = !isCloudSun;
   cloudImg.src = isCloudSun ? 'static/assets/CloudSun.svg' : 'static/assets/CloudMoon.svg';
-  logoImg.src = isCloudSun ? 'static/assets/Logo2.svg' : 'static/assets/Logo.svg';
   dnaImg.src = isCloudSun ? 'static/assets/dna-icon-dark.svg' : 'static/assets/dna-icon.svg';
   phylogenyImg.src = isCloudSun ? 'static/assets/phylogeny-dark.svg' : 'static/assets/phylogeny.svg';
   emailImg.src = isCloudSun ? 'static/assets/email-icon-dark.svg' : 'static/assets/email-icon-light.svg';
@@ -27,4 +27,12 @@ replaceConst.addEventListener('click', function(){
   iconHelps.forEach(function(iconhelp) {
     iconhelp.src = isCloudSun ? 'static/assets/QuestionDiamond-dark.svg' : 'static/assets/QuestionDiamond-light.svg';
   });
+
+  shdownloads.forEach(function(shdownload){
+    shdownload.src = isCloudSun ? 'static/assets/download.svg' : 'static/assets/icon-download.svg';
+  });
+
+  logoImgs.forEach(function(logoImg){
+    logoImg.src = isCloudSun ? 'static/assets/Logo2.svg' : 'static/assets/Logo.svg';
+  })
 });
